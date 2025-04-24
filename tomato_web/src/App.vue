23f21955b9174ac80aed5c232c47775f0b3aad9c
WENTE <manqing321@gmail.com>
2025-04-24 21:12:05 +0800

    init this website project

---
+++ tomato_web/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+   <h3>hello welcome to my tomato clock website!</h3>
+   <h3>here are some func list</h3>
+   <ul>
+    <li>tomato clock: we need a clock to display the time.</li>
+    <li>animation: maybe an gif or animation is nice for user friendly.</li>
+    <li>data base: website should save user's recond to database.</li>
+   </ul>
+</template>
+
+<style scoped>
+</style>
