20c109f6d960d207d5c4ad25ae88f8b5504523ee
WENTE <manqing321@gmail.com>
2025-07-24 20:09:16 +0800

    delete simple animation

--- tomato_web/src/App.vue
+++ tomato_web/src/App.vue
@@ -3,23 +3,7 @@
 
 <template>
   <div class="app-container">
-    <h3>hello welcome to my tomato clock website!</h3>
-    <h3>here are some func list</h3>
-    <ul>
-      <li>tomato clock: we need a clock to display the time.</li>
-      <li>animation: maybe an gif or animation is nice for user friendly.</li>
-      <li>data base: website should save user's record to database.</li>
-    </ul>
-    <div class="tomato-container">
-      <div class="tomato-plant">
-        <div class="stem"></div>
-        <div class="leaf leaf-left"></div>
-        <div class="leaf leaf-right"></div>
-        <div class="flower"></div>
-        <div class="tomato"></div>
-      </div>
-    </div>
+     
   </div>
 </template>
-
-<style src="./styles/App.css"></style>
+ 
