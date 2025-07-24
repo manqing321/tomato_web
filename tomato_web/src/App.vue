b1cef65f329b63998e4f61ea34e5cd796822e21f
WENTE <manqing321@gmail.com>
2025-07-24 20:39:20 +0800

    add tomato button

--- tomato_web/src/App.vue
+++ tomato_web/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+const topic = ref('')
+const addTomato = () => {
+  console.log(topic.value)
+}
 </script>
 
 <template>
-  <div class="app-container">
-     
+  <div style="display: flex; width: 30%;">
+    <el-input v-model="topic" placeholder="Topic"/>
+    <el-button @click="addTomato" style="margin-left: 2%;" type="success">add tomato</el-button>
   </div>
 </template>
  
