99aa43d898478ed914146b3eafc43a8f4bf0194e
WENTE <manqing321@gmail.com>
2025-07-24 20:48:35 +0800

    add beg-end time display

--- tomato_web/src/App.vue
+++ tomato_web/src/App.vue
@@ -1,7 +1,20 @@
 <script setup>
 const topic = ref('')
+const timeinfo = ref('')
 const addTomato = () => {
-  console.log(topic.value)
+  
+    const now = new Date()
+    const endTime = new Date(now.getTime() + 30 * 60 * 1000)
+    
+    const formatTime = (date) => {
+      return date.toLocaleTimeString('zh-CN', { 
+        hour: '2-digit', 
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    }
+    
+    timeinfo.value = `【${topic.value}】 from ${formatTime(now)} to ${formatTime(endTime)}`
 }
 </script>
 
@@ -9,6 +22,9 @@ const addTomato = () => {
   <div style="display: flex; width: 30%;">
     <el-input v-model="topic" placeholder="Topic"/>
     <el-button @click="addTomato" style="margin-left: 2%;" type="success">add tomato</el-button>
+  </div>
+  <div>
+    <h5>{{ timeinfo }}</h5>
   </div>
 </template>
  
